4b1252d6f6a376ba5979ebd5f8a451b048adf764
Chris Ancheta <anchetc@aklc.govt.nz>
2024-11-22 16:22:38 +1300

    Added Another graph

--- report/views.py
+++ report/views.py
@@ -4,20 +4,27 @@ from django.db.models import Count
 import json
 
 def index(request):
-    labels = []  # This will hold the names of the people being visited (x-axis)
-    data = []    # This will hold the count of visits for each person (y-axis)
+    # Count visits for each person being visited
+    visitor_query = Visitor.objects.values('person_to_visit__name') \
+                                   .annotate(visit_count=Count('person_to_visit')) \
+                                   .order_by('-visit_count')
 
-    # Query to count the number of visits for each 'person_to_visit'
-    queryset = Visitor.objects.values('person_to_visit__name') \
-                               .annotate(visit_count=Count('person_to_visit')) \
-                               .order_by('-visit_count')
+    # Count visits for each date
+    date_query = Visitor.objects.values('login_date') \
+                                .annotate(visit_count=Count('login_date')) \
+                                .order_by('login_date')
 
-    # Prepare labels and data for the chart
-    for entry in queryset:
-        labels.append(entry['person_to_visit__name'])  # The name of the person being visited (x-axis label)
-        data.append(entry['visit_count'])  # Count of visits for this person (y-axis value)
+    # Prepare the data for the first chart (person visits)
+    labels_visitors = [entry['person_to_visit__name'] for entry in visitor_query]
+    data_visitors = [entry['visit_count'] for entry in visitor_query]
+
+    # Prepare the data for the second chart (date visits)
+    labels_dates = [entry['login_date'].strftime('%Y-%m-%d') for entry in date_query]
+    data_dates = [entry['visit_count'] for entry in date_query]
 
     return render(request, 'report/index.html', {
-        'labels': json.dumps(labels),
-        'data': json.dumps(data),
+        'labels_visitors': json.dumps(labels_visitors),
+        'data_visitors': json.dumps(data_visitors),
+        'labels_dates': json.dumps(labels_dates),
+        'data_dates': json.dumps(data_dates),
     })
